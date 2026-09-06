74d90802d933b39d4a794f39d56e3de55e3f1f8c
ARRRRNY <ahmettok@gmail.com>
2026-09-06 18:19:04 +0300

    fix(320): add profileId param to _FakePlatformProxyController.clearProxyOverride
    The fake class was missing the {String? profileId} named parameter that the
    platform interface requires, causing a compile error in the test suite.
    
    Closes #320

--- zikzak_inappwebview/test/proxy_tracing_controllers_test.dart
+++ zikzak_inappwebview/test/proxy_tracing_controllers_test.dart
@@ -21,7 +21,7 @@ class _FakePlatformProxyController extends PlatformProxyController {
   }
 
   @override
-  Future<void> clearProxyOverride() async {
+  Future<void> clearProxyOverride({String? profileId}) async {
     clearOverrideCount++;
   }
 
